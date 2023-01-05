beb2ba24aac0fed7f174340717ae6a8341fdb738
Prasanna <prasannakb440@gmail.com>
2023-01-05 13:26:39 +0545

    modified saveUser controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const joi = require('joi')
+const bcrypt = require('bcryptjs')
 
 exports.saveUser = async (req, res, next) => {
 
@@ -26,5 +27,32 @@ exports.saveUser = async (req, res, next) => {
     if (error) {
         return next(error)
     }
-    res.status(201).json({ message: "Registered Successfully!!!" })
+
+    let hashedPassword = await bcrypt.hash(req.body.password, 10)
+    let jsonObject = {
+        first_name: req.body.first_name,
+        middle_name: req.body.middle_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        phone: req.body.phone,
+        username: req.body.username,
+        password: hashedPassword,
+    }
+
+
+    try {
+        //check if email already exists
+        const user = await User.findOne({ where: { email: req.body.email } })
+
+        if (user === null) {
+            //create new user
+            const user = await User.create(jsonObject)
+            res.status(201).json({ message: "Registered Successfully!!!" })
+        } else {
+            res.status(200).json({ message: "This email is already registered." })
+        }
+    } catch (error) {
+        return next(error)
+    }
+
 }
